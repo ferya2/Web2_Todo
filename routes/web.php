09dc6ac82bb25677e49e5864cc9457b36727f052
ferya2 <adefery221@gmail.com>
2024-06-18 13:51:25 +0700

    laravel auth

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TodoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('/register', [UserController::class, 'register']);
+    Route::get('/login', [UserController::class, 'login'])->name('login');
+    Route::post('/login/auth', [UserController::class, 'loginAuth']);
+    Route::post('/register/store', [UserController::class, 'storeRegister']);
+    Route::post('/logout', [UserController::class, 'logout']);
+});
+
+Route::prefix('dashboard')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+});
+
+Route::prefix('todo')->group(function () {
+    Route::get('/', [TodoController::class, 'index']);
+    Route::get('/create', [TodoController::class, 'create']);
+    Route::get('/edit/{id}', [TodoController::class, 'edit']);
+    Route::get('/delete/{id}', [TodoController::class, 'destroy']);
+    Route::post('/store', [TodoController::class, 'store']);
+    Route::post('/update/{id}', [TodoController::class, 'update']);
+});
